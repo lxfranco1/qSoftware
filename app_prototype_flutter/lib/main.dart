cf0dc1b334f67af05b731edbf7b1c161d0d3eb6a
TheNextCEO <charles.benji33@gmail.com>
2019-11-20 04:35:02 -0500

    Added More Login Stuff

--- app_prototype_flutter/lib/main.dart
+++ app_prototype_flutter/lib/main.dart
@@ -54,6 +54,13 @@ class HomeController extends StatelessWidget {
 
 class Provider extends InheritedWidget {
   final AuthService auth;
-  Provider({Key key, Widget child, this.auth})
+  Provider({Key key, Widget child, this.auth,}) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget){
+    return true;
+  }
+
+  static Provider of(BuildContext context) => (context.inheritFromWidgetOfExactType(Provider)) as Provider;
 
 }
--- app_prototype_flutter/lib/views/sign_up_view.dart
+++ app_prototype_flutter/lib/views/sign_up_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:auto_size_text/auto_size_text.dart";
@@ -10,8 +12,9 @@ class SignUpView extends StatefulWidget {
   final AuthFormType authFormType;
 
   SignUpView({Key key, @required this.authFormType}) : super(key: key);
+
   @override
-  _SignUpViewState createState() => _SignUpViewState();
+  _SignUpViewState createState() => _SignUpViewState(authFormType: this.authFormType);
 }
 
 class _SignUpViewState extends State<SignUpView> {
@@ -20,6 +23,20 @@ class _SignUpViewState extends State<SignUpView> {
 
   final formKey = GlobalKey<FormState>();
   String _email, _password, _name;
+
+  void switchFormState(String state){
+    formKey.currentState.reset();
+    if(state == "signUp"){
+      setState(() {
+        authFormType = AuthFormType.signUp;
+      });
+    }
+    else{
+      setState(() {
+        authFormType = AuthFormType.signIn;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,22 +54,14 @@ class _SignUpViewState extends State<SignUpView> {
           child: Column(
             children: <Widget>[
               SizedBox(height: _height * 0.05),
-              AutoSizeText(
-                "SignUp",
-                maxLines: 1,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.white,
-                ),
-              ),
+              buildHeaderText(),
               SizedBox(height: _height * 0.05),
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Form(
                   key: formKey,
                   child: Column(
-                    children: buildInputs(),
+                    children: buildInputs() + buildButtons(),
                   ),
                 ),
               )
@@ -63,9 +72,39 @@ class _SignUpViewState extends State<SignUpView> {
     );
   }
 
+  AutoSizeText buildHeaderText() {
+    String _headerText;
+    if(authFormType == AuthFormType.signUp){
+      _headerText = "Create New Account";
+    }
+    else{
+      _headerText = "Sign In";
+
+    }
+    return AutoSizeText(
+      _headerText,
+      maxLines: 1,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 35,
+        color: Colors.white,
+      ),
+    );
+  }
+
   List<Widget> buildInputs(){
     List<Widget> textFields = [];
     // if were in the sign up state add name
+    if(authFormType == AuthFormType.signUp){
+      textFields.add(
+        TextFormField(
+          style: TextStyle(fontSize: 22.0),
+          decoration: buildSignUpInputDecoration("Name"),
+          onSaved: (value) => _name = value,
+        ),
+      );
+      textFields.add(SizedBox(height: 20,));
+    }
     
     // add email & password
     textFields.add(
@@ -98,5 +137,26 @@ class _SignUpViewState extends State<SignUpView> {
         enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 0.0)),
         contentPadding: const EdgeInsets.only(left: 14.0, bottom: 10.0, top: 10.0),
       );
+  }
+
+  List<Widget> buildButtons(){
+    String _switchButtonText, _newFormState;
+
+    if(authFormType == AuthFormType.signIn){
+      _switchButtonText = "Create New Account";
+      _newFormState = "signUp";
+    } else{
+      _switchButtonText = "Have an account? Sign In";
+      _newFormState = "signIn";
+    }
+
+    return [
+      FlatButton(
+          child: Text(_switchButtonText, style: TextStyle(color: Colors.white),),
+        onPressed: (){
+            switchFormState(_newFormState);
+        },
+      )
+    ];
   }
 }
