651c6d7ff521ed4f03f4b4a0e647a03bb499ce70
Alexander J Franco <lxfranco1@gmail.com>
2019-11-27 13:59:53 -0500

    IOS Support Test

--- app_prototype_flutter/ios/Flutter/flutter_export_environment.sh
+++ app_prototype_flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/artemis/Downloads/flutter_linux_v1.9.1+hotfix.6-stable/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/artemis/StudioProjects/qSoftware_1/app_prototype_flutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/alexanderfranco/StudioProjects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alexanderfranco/StudioProjects/qSoftware/app_prototype_flutter"
+export "FLUTTER_TARGET=/Users/alexanderfranco/StudioProjects/qSoftware/app_prototype_flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/artemis/Downloads/flutter_linux_v1.9.1+hotfix.6-stable/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/alexanderfranco/StudioProjects/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
