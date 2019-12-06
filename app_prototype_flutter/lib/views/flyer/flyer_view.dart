1422465e4476c0e501286f9314ce388b871467d4
jmckenzie97 <49374308+jmckenzie97@users.noreply.github.com>
2019-12-05 20:56:46 -0500

    making share work again

--- app_prototype_flutter/lib/views/flyer/flyer_view.dart
+++ app_prototype_flutter/lib/views/flyer/flyer_view.dart
@@ -23,109 +23,171 @@ class _FlyerState extends State<Flyer> {
   static const double _hPad = 16.0;
   @override
   Widget build(BuildContext context) {
+    String title = widget.event.title;
+    String date = widget.event.date.toString();
+    String eventType;
+    String creatorID;
+    if (widget.event.eventType == null){
+      eventType = "Event";
+    } else {
+      eventType = widget.event.eventType;
+    }
+    if (widget.event.creatorsID == null){
+      creatorID = "ID";
+    } else {
+      creatorID = widget.event.creatorsID;
+    }
+    String description = widget.event.description;
+    int attendance = widget.event.attendance;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Event'),
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
-              child: TextSection(widget.event.title),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
-              child: TextSection("Date"),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
-              child: TextSection("EventType"),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
-              child: TextSection("Username"),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 5.0, 205, 4.0),
-              child: MaterialButton(
-                color: Colors.blueAccent,
-                highlightColor: Colors.blueGrey,
-                height: 48,
-                minWidth: 80.0,
-                elevation: 8,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                ),
-
-                child:
-                Text('Follow User', style: new TextStyle(fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-                ),
-
-                onPressed: () {
-//                  Navigator.push(
-//                    context,
-//                    MaterialPageRoute(builder: (context) => SecondRoute()),);
-                },
+      body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
+                child: TextSection(title),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 34.0, _hPad, 4.0),
-              child: TextSection("Description"),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 14.0, _hPad, 4.0),
-              child: TextSection("X people attending"),
-            ),
-
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 14.0, 205, 4.0),
-              child: MaterialButton(
-                color: Colors.blueAccent,
-                highlightColor: Colors.blueGrey,
-                height: 48.0,
-                minWidth: 30.0,
-                elevation: 8,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                ),
-
-                child:
-                Text('Attend Event', style: new TextStyle(fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-                ),
-
-                onPressed: () {
-//                  Navigator.push(
-//                    context,
-//                    MaterialPageRoute(builder: (context) => SecondRoute()),);
-                },
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
+                child: TextSection(date),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4.0, 34.0, _hPad, 4.0),
-              alignment: Alignment.center,
-              child: TextSection("Rate This Event"),
-            ),
-            Container(
-              //padding: const EdgeInsets.fromLTRB(4.0, 8.0, _hPad, 4.0),
-                alignment: Alignment.center,
-                child: SmoothStarRating(
-                  rating: rating,
-                  size: 45,
-                  starCount: 5,
-                  spacing: 1.5,
-                  onRatingChanged: (value) {
-                    setState(() {
-                      rating = value;
-                    });
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
+                child: TextSection(eventType),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 0.0, _hPad, 4.0),
+                child: TextSection(creatorID),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 5.0, 205, 4.0),
+                child: MaterialButton(
+                  color: Colors.blueAccent,
+                  highlightColor: Colors.blueGrey,
+                  height: 48,
+                  minWidth: 80.0,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                  child:
+                  Text('Follow User', style: new TextStyle(fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                  ),
+
+                  onPressed: () {
+  //                  Navigator.push(
+  //                    context,
+  //                    MaterialPageRoute(builder: (context) => SecondRoute()),);
                   },
-                )
-            )
-          ]
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 34.0, _hPad, 4.0),
+                child: TextSection(description),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 14.0, _hPad, 4.0),
+                child: TextSection(attendance.toString() + " people attending"),
+              ),
+
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 14.0, 205, 4.0),
+                child: MaterialButton(
+                  color: Colors.blueAccent,
+                  highlightColor: Colors.blueGrey,
+                  height: 48.0,
+                  minWidth: 30.0,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                  child:
+                  Text('Attend Event', style: new TextStyle(fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                  ),
+
+                  onPressed: () {
+  //                  Navigator.push(
+  //                    context,
+  //                    MaterialPageRoute(builder: (context) => SecondRoute()),);
+                  },
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 34.0, _hPad, 4.0),
+                alignment: Alignment.center,
+                child: TextSection("Rate This Event"),
+              ),
+              Container(
+                //padding: const EdgeInsets.fromLTRB(4.0, 8.0, _hPad, 4.0),
+                  alignment: Alignment.center,
+                  child: SmoothStarRating(
+                    rating: rating,
+                    size: 45,
+                    starCount: 5,
+                    spacing: 1.5,
+                    onRatingChanged: (value) {
+                      setState(() {
+                        rating = value;
+                      });
+                    },
+                  )
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 14.0, 205, 4.0),
+                child: MaterialButton(
+                  color: Colors.blueAccent,
+                  highlightColor: Colors.blueGrey,
+                  height: 48.0,
+                  minWidth: 30.0,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                  child:
+                  Text('Share with email/SMS', style: new TextStyle(fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                  ),
+
+                  onPressed: () => share(context, widget.event)
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4.0, 14.0, 205, 4.0),
+                child: MaterialButton(
+                  color: Colors.blueAccent,
+                  highlightColor: Colors.blueGrey,
+                  height: 48.0,
+                  minWidth: 30.0,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                  child:
+                  Text('Share with Twitter', style: new TextStyle(fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                  ),
+
+                  onPressed: () async {
+                    var response = await FlutterShareMe().shareToTwitter(
+                        msg: "Come to my event: " + widget.event.title + " at " + widget.event.toString() );
+                  },
+                ),
+              ),
+            ]
       ),
+    ),
     );
   }
   void share(BuildContext context, Event event){
